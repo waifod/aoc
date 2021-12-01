530f832268f41828ad4fd7e01d024ad500055259
Waifod <signupwaifod@protonmail.com>
2021-12-01 12:02:45 +0100

    aoc21-01

---
+++ aoc21/aoc21-01a.py
@@ -0,0 +1,19 @@
+import sys
+
+name = 'aoc21-01.txt'
+
+def format(name: str) -> list[int]:
+    lines = open(name, 'r').readlines()
+    ints = [int(ln) for ln in lines]
+    return ints
+
+def count_increases(ls: list[int]) -> int:
+    count = 0
+    for i in range(len(ls) - 1):
+        count += ls[i] < ls[i + 1]
+    return count
+
+if __name__ == '__main__':
+    ints = format(name)
+    increases = count_increases(ints)
+    print(increases)
