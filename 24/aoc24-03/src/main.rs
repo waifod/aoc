e97254264316890ea11b186573d13afe6a076729
waifod <waifod@pm.me>
2024-12-03 20:20:45 +0100

    Finish y24d3

--- 24/aoc24-03/src/main.rs
+++ 24/aoc24-03/src/main.rs
@@ -1,26 +1,40 @@
 use regex::Regex;
-use utils;
+use std::fs;
 
 const FILE_PATH: &str = "./input/input.txt";
 
-fn parse_input(file_path: &str) -> Vec<String> {
-    utils::read_lines(file_path).flatten().map(|line|
-        String::from(line)
-    ).collect()
+fn parse_input(file_path: &str) -> String {
+    fs::read_to_string(file_path).unwrap()
 }
 
-fn solve_a(input: &Vec<String>) -> i32 {
+fn solve_a(input: &str) -> usize {
     let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-    input.into_iter()
-        .map(|line| re.captures_iter(line))
-        .flatten()
+    re.captures_iter(input)
         .map(|c| c.extract())
-        .map(|(_,[m1,m2])| m1.parse::<i32>().unwrap() * m2.parse::<i32>().unwrap())
+        .map(|(_,[m1,m2])| m1.parse::<usize>().unwrap() * m2.parse::<usize>().unwrap())
         .sum()
 }
 
-fn solve_b(input: &Vec<String>) -> usize {
-    0
+fn solve_b(input: &str) -> usize {
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut enabled = true;
+
+    re.captures_iter(input)
+        .filter(|capture| {
+            let fst = capture.get(0).unwrap().as_str();
+            if fst == "do()" {
+                enabled = true;
+                return false;
+            } else if fst == "don't()" {
+                enabled = false;
+            }
+            enabled
+        })
+        .map(|capture|
+            capture.get(1).unwrap().as_str().parse::<usize>().unwrap() 
+                * capture.get(2).unwrap().as_str().parse::<usize>().unwrap()
+        )
+        .sum()
 }
 
 fn main() {
